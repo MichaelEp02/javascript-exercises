4e08ae4843d460488c5b312777d72b4500fe406e
Michael Epstein <miep@home.home>
2024-06-21 14:30:02 -0400

    completed 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+   if(temp == 32)
+    return 0;
+  return Math.round(((temp -32) / 1.8) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round((temp * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
