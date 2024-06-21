9683584662a4bf249cc541188627c7bef2f9a8d5
Michael Epstein <miep@home.home>
2024-06-21 12:02:59 -0400

    completed 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const varr = [];
+    loop1:
+    for (val of arr){
+        for(cval of args){
+            if(val === cval){
+                continue loop1;
+            }
+        }
+        varr.push(val)
+    }
+    return varr;
 };
 
 // Do not edit below this line
