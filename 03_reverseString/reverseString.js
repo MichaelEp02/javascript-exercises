0d7814494e4eb9f51306b93151674dd5677738b9
Michael Epstein <miep@home.home>
2024-06-21 11:00:12 -0400

    finished 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   let res = ''
+   for(i=0; i<str.length; i++){
+        res += str[str.length - 1 -i]
+   } 
+   return res;
 };
 
 // Do not edit below this line
